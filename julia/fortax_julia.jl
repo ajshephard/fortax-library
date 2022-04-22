a229ee17276b8f42fd967dad5df14865474c0314
Andrew Shephard <andrew.james.shephard@gmail.com>
2022-04-22 14:13:51 -0400

    preprocessing for julia and julia interface

--- julia/fortax_julia.jl
+++ julia/fortax_julia.jl
@@ -1,114 +1,165 @@
 using StaticArrays
-using Parameters
-using Setfield
 
-const fortaxlib="fortax.so"
+const fortaxlib="/path/to/fortax.so"
 
-const maxRPI = 1024
-const maxkids = 10
+include("fortax_julia_type.jl")
 
-const lab_ctax = (banda = 1, bandb = 2, bandc = 3, bandd = 4, 
-                  bande = 5, bandf = 6, bandg = 7, bandh = 8)
+# wrappers for c bindings
 
-const lab_tenure = (own_outright = 1, mortgage = 2, part_own = 3,
-                    social_renter = 4, private_renter = 5, rent_free = 6,
-                    other = 7)
+# fortax_calc
 
-const lab_region = (north_east = 1, north_west = 2, yorks = 3, east_midlands = 4,
-                    west_midlands = 5, eastern = 6, london = 7, south_east = 8,
-                    south_west = 9, wales = 10, scotland = 11, northern_ireland = 12)
-
-@with_kw struct famad_t
-    age::Cint = 25
-    selfemp::Bool = false
-    hrs::Cdouble = 0.0
-    earn::Cdouble = 0.0
+function calcNetInc(sys::sys_t, fam::fam_t)
+    return ccall((:C_FORTAX_calcNetInc, fortaxlib), net_t, (Ref{sys_t}, Ref{fam_t}), sys, fam)
 end
 
-@with_kw struct fam_t
-    couple::Bool = false
-    married::Bool = false
-    ccexp::Cdouble = 0.0
-    maint::Cdouble = 0.0
-    nkids::Cint = 0
-    kidage::SVector{maxkids, Cint} = zeros(maxkids)
-    yngkid::Cint = 0
-    nothads::Cint = 0
-    tenure::Cint = lab_tenure.own_outright
-    rent::Cdouble = 0.0
-    rentcap::Cdouble = 0.0
-    region::Cint = lab_region.north_east
-    ctband::Cint = lab_ctax.bandd
-    banddratio::Cdouble = 1.0
-    intdate::Cint = 19900101
-    ad::SVector{2, famad_t} = @SVector [famad_t() for _ = 1:2]
-    #ad::NTuple{2, famad_t} = (famad_t(), famad_t())
+# fortax_extra
+
+function setMinAmount(sys::sys_t, minamt)
+    sysref = Ref{sys_t}(sys)
+    ccall((:C_FORTAX_setMinAmount, fortaxlib), Cvoid, (Ref{sys_t}, Ref{Cdouble}), sysref, minamt)
+    return sysref[]
 end
 
-@with_kw struct netad_t
-    taxable::Cdouble
-    inctax::Cdouble
-    natins::Cdouble
-    natinsc1::Cdouble
-    natinsc2::Cdouble
-    natinsc4::Cdouble
-    pretaxearn::Cdouble
-    posttaxearn::Cdouble
+function abolishNIFee(sys::sys_t)
+    sysref = Ref{sys_t}(sys)
+    ccall((:C_FORTAX_abolishNIFee, fortaxlib), Cvoid, (Ref{sys_t},), sysref)
+    return sysref[]
+end
+ 
+function disableTaperRounding(sys::sys_t)
+    sysref = Ref{sys_t}(sys)
+    ccall((:C_FORTAX_disableTaperRounding, fortaxlib), Cvoid, (Ref{sys_t},), sysref)
+    return sysref[]
 end
 
-@with_kw struct nettu_t
-    pretaxearn::Cdouble
-    posttaxearn::Cdouble
-    chben::Cdouble
-    matgrant::Cdouble
-    fc::Cdouble
-    wtc::Cdouble
-    ctc::Cdouble
-    ccexp::Cdouble
-    incsup::Cdouble
-    hben::Cdouble
-    polltax::Cdouble
-    polltaxben::Cdouble
-    ctax::Cdouble
-    ctaxben::Cdouble
-    maxuc::Cdouble
-    uc::Cdouble
-    dispinc::Cdouble
-    pretax::Cdouble
-    nettax::Cdouble
-    chcaresub::Cdouble
-    fsm::Cdouble
-    totben::Cdouble
+function fsMinAppAmt(sys::sys_t, inappamt)
+    sysref = Ref{sys_t}(sys)
+    c_inappamt = Cint(inappamt)
+    ccall((:C_FORTAX_fsMinAppAmt, fortaxlib), Cvoid, (Ref{sys_t}, Ref{Cint}), sysref, c_inappamt)
+    return sysref[]
 end
 
-@with_kw struct net_t
-    ad::SVector{2, netad_t} = @SVector [netad_t() for _ = 1:2]
-    tu::nettu_t = nettu_t()
+function taperMatGrant(sys::sys_t, taper)
+    sysref = Ref{sys_t}(sys)
+    c_taper = Cint(taper)
+    ccall((:C_FORTAX_taperMatGrant, fortaxlib), Cvoid, (Ref{sys_t}, Ref{Cint}), sysref, c_taper)
+    return sysref[]
 end
 
-@with_kw struct rpi_t
-    ndate::Cint = 0
-    date::SVector{maxRPI, Cint} = zeros(maxRPI)
-    index::SVector{maxRPI, Cdouble} = ones(maxRPI)
+function imposeUc(sys::sys_t)
+    sysref = Ref{sys_t}(sys)
+    ccall((:C_FORTAX_imposeUc, fortaxlib), Cvoid, (Ref{sys_t},), sysref)
+    return sysref[]
 end
 
-function fam_desc(fam::fam_t)
-    ccall((:C_FORTAX_fam_desc, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{Cvoid}, Ptr{Cvoid}), fam, C_NULL, C_NULL)
+# fortax_kinks
+
+function evalKinksHours(bc::bcout_t, hours)
+    earn = Ref{Cdouble}()
+    net = Ref{Cdouble}()
+    mtr = Ref{Cdouble}()
+    ccall((:C_FORTAX_evalKinksHours, fortaxlib), Cvoid, (Ref{bcout_t}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ptr{Cvoid}, Ptr{Cvoid}), 
+                                                  bc, hours, earn, net, mtr, C_NULL, C_NULL)
+    return earn[], net[], mtr[]
 end
 
-function fam_desc(fam::fam_t, fname::String)
-    len_fname = length(fname)
-    ccall((:C_FORTAX_fam_desc, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{UInt8}, Ref{Cint}), fam, fname, len_fname)
+function evalKinksEarn(bc::bcout_t, earn)
+    hours = Ref{Cdouble}()
+    net = Ref{Cdouble}()
+    mtr = Ref{Cdouble}()    
+    ccall((:C_FORTAX_evalKinksEarn, fortaxlib), Cvoid, (Ref{bcout_t}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ptr{Cvoid}, Ptr{Cvoid}), 
+                                                  bc, earn, hours, net, mtr, C_NULL, C_NULL)
+    return hours[], net[], mtr[]
 end
 
-function net_desc(net::net_t)
-    ccall((:C_FORTAX_net_desc, fortaxlib), Cvoid, (Ref{net_t}, Ptr{Cvoid}, Ptr{Cvoid}), net, C_NULL, C_NULL)
+function kinksHours(sys::sys_t, fam::fam_t, ad, wage, hours1, hours2)
+    ad_c = Cint(ad)
+    return ccall((:C_FORTAX_kinksHoursDefault, fortaxlib), bcout_t, (Ref{sys_t}, Ref{fam_t}, Ref{Cint}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}), 
+                 sys, fam, ad_c, wage, hours1, hours2)
 end
 
-function net_desc(net::net_t, fname::String)
-    len_fname = length(fname)
-    ccall((:C_FORTAX_net_desc, fortaxlib), Cvoid, (Ref{net_t}, Ptr{UInt8}, Ref{Cint}), net, fname, len_fname)
+function kinksHours(sys::sys_t, fam::fam_t, ad, wage, hours1, hours2, taxlevel, taxout)
+
+    # convert taxout (vector of strings) to a matrix of char
+    len_taxout = Cint(length(taxout))
+    mat_taxout = zeros(Cchar, 16, len_taxout)
+    for i in 1:len_taxout
+        for (j, c) in enumerate(taxout[i])
+            mat_taxout[j, i] = c
+        end
+    end
+    len_taxlevel = Cint(length(taxlevel))
+    ad_c = Cint(ad)
+    return ccall((:C_FORTAX_kinksHours, fortaxlib), bcout_t, (Ref{sys_t}, Ref{fam_t}, Ref{Cint}, 
+                 Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ptr{UInt8}, Ref{Cint}, Ptr{UInt8}, Ref{Cint}), 
+                 sys, fam, ad_c, wage, hours1, hours2, taxlevel, len_taxlevel, mat_taxout, len_taxout)
 end
+
+function kinksEarn(sys::sys_t, fam::fam_t, ad, hours, earn1, earn2)
+    ad_c = Cint(ad)
+    return ccall((:C_FORTAX_kinksEarnDefault, fortaxlib), bcout_t, (Ref{sys_t}, Ref{fam_t}, Ref{Cint}, Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}), 
+                 sys, fam, ad_c, hours, earn1, earn2)
+end
+
+function kinksEarn(sys::sys_t, fam::fam_t, ad_c, hours, earn1, earn2, taxlevel, taxout)
+
+    # convert taxout (vector of strings) to a matrix of char
+    len_taxout = Cint(length(taxout))
+    mat_taxout = zeros(Cchar, 16, len_taxout)
+    for i in 1:len_taxout
+        for (j, c) in enumerate(taxout[i])
+            mat_taxout[j, i] = c
+        end
+    end
+    len_taxlevel = Cint(length(taxlevel))
+    ad_c = Cint(ad)
+    return ccall((:C_FORTAX_kinksEarn, fortaxlib), bcout_t, (Ref{sys_t}, Ref{fam_t}, Ref{Cint}, 
+                 Ref{Cdouble}, Ref{Cdouble}, Ref{Cdouble}, Ptr{UInt8}, Ref{Cint}, Ptr{UInt8}, Ref{Cint}), 
+                 sys, fam, ad_c, hours, earn1, earn2, taxlevel, len_taxlevel, mat_taxout, len_taxout)
+end
+
+function kinks_desc(bcout::bcout_t)
+    ccall((:C_FORTAX_kinks_desc, fortaxlib), Cvoid, (Ref{bcout_t},), bcout)
+end
+
+# fortax_prices
+
+function loadIndex(fname::String)
+    rpiref = Ref{rpi_t}()
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_loadIndex, fortaxlib), Cvoid, (Ref{rpi_t}, Ptr{UInt8}, Ref{Cint}), rpiref, fname, len_fname)
+    return rpiref[]
+end
+
+function uprateFactor(rpi::rpi_t, date0, date1)
+    date0c = Cint(date0)
+    date1c = Cint(date1)
+    return ccall((:C_FORTAX_uprateFactor, fortaxlib), Cdouble, (Ref{rpi_t}, Ref{Cint}, Ref{Cint}), rpi, date0c, date1c)
+end
+
+function uprateSys(sys::sys_t, factor)
+    sysref = Ref{sys_t}(sys)
+    ccall((:C_FORTAX_uprateSys, fortaxlib), Cvoid, (Ref{sys_t}, Ref{Cdouble}, Ptr{Cvoid}), sysref, factor, C_NULL)
+    return sysref[]
+end
+
+function uprateSys(sys::sys_t, factor, newdate)
+    sysref = Ref{sys_t}(sys)
+    c_newdate = Cint(newdate)
+    ccall((:C_FORTAX_uprateSys, fortaxlib), Cvoid, (Ref{sys_t}, Ref{Cdouble}, Ptr{Cint}), sysref, factor, c_newdate)
+    return sysref[]
+end
+
+# fortax_read
+
+function readFortaxParams(fname::String)
+    sysref = Ref{sys_t}()
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_readFortaxParams, fortaxlib), Cvoid, (Ref{sys_t}, Ptr{UInt8}, Ref{Cint}, Ptr{Cvoid}), sysref, fname, len_fname, C_NULL)
+    return sysref[]
+end
+
+# fortax_type
 
 function fam_init()
     famref = Ref{fam_t}()
@@ -122,23 +173,54 @@ function net_init()
     return netref[]
 end
 
-# fortax_prices
-
-function load_index()
-    rpiref = Ref{rpi_t}()
-    ccall((:C_FORTAX_loadIndex, fortaxlib), Cvoid, (Ref{rpi_t}, Ptr{Cvoid}, Ptr{Cvoid}), rpiref, C_NULL, C_NULL)
-    return rpiref[]
+function sys_init()
+    sysref = Ref{sys_t}()
+    ccall((:C_FORTAX_sys_init, fortaxlib), Cvoid, (Ref{sys_t}, ), sysref)
+    return sysref[]
 end
 
-function load_index(fname::String)
-    rpiref = Ref{rpi_t}()
+function fam_saveF90(fam::fam_t)
+    ccall((:C_FORTAX_fam_saveF90, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{Cvoid}, Ptr{Cvoid}), fam, C_NULL, C_NULL)
+end
+
+function fam_saveF90(fam::fam_t, fname::String)
     len_fname = length(fname)
-    ccall((:C_FORTAX_loadIndex, fortaxlib), Cvoid, (Ref{rpi_t}, Ptr{UInt8}, Ref{Cint}), rpiref, fname, len_fname)
-    return rpiref[]
+    ccall((:C_FORTAX_fam_saveF90, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{UInt8}, Ref{Cint}), fam, fname, len_fname)
 end
 
-function uprate_factor(rpi::rpi_t, date0::Integer, date1::Integer)
-    date0c = Cint(date0)
-    date1c = Cint(date1)
-    return ccall((:C_FORTAX_uprateFactor, fortaxlib), Cdouble, (Ref{rpi_t}, Ref{Cint}, Ref{Cint}), rpi, date0c, date1c)
+function fam_desc(fam::fam_t)
+    ccall((:C_FORTAX_fam_desc, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{Cvoid}, Ptr{Cvoid}), fam, C_NULL, C_NULL)
+end
+
+function fam_desc(fam::fam_t, fname::String)
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_fam_desc, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{UInt8}, Ref{Cint}), fam, fname, len_fname)
+end
+
+function net_desc(net::net_t)
+    ccall((:C_FORTAX_net_desc, fortaxlib), Cvoid, (Ref{net_t}, Ptr{Cvoid}, Ptr{Cvoid}), net, C_NULL, C_NULL)
+end
+
+function net_desc(net::net_t, fname::String)
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_net_desc, fortaxlib), Cvoid, (Ref{net_t}, Ptr{UInt8}, Ref{Cint}), net, fname, len_fname)
+end
+
+function fam_desc(fam::fam_t, fname::String)
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_fam_desc, fortaxlib), Cvoid, (Ref{fam_t}, Ptr{UInt8}, Ref{Cint}), fam, fname, len_fname)
+end
+
+function printFortaxParams(sys::sys_t)
+    ccall((:C_FORTAX_printFortaxParams, fortaxlib), Cvoid, (Ref{sys_t}, Ptr{Cvoid}, Ptr{Cvoid}), sys, C_NULL, C_NULL)
+end
+
+function printFortaxParams(sys::sys_t, fname::String)
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_printFortaxParams, fortaxlib), Cvoid, (Ref{sys_t}, Ptr{UInt8}, Ref{Cint}), sys, fname, len_fname)
+end
+
+function writeFortaxParams(sys::sys_t, fname::String)
+    len_fname = Cint(length(fname))
+    ccall((:C_FORTAX_writeFortaxParams, fortaxlib), Cvoid, (Ref{sys_t}, Ptr{UInt8}, Ref{Cint}), sys, fname, len_fname)
 end
